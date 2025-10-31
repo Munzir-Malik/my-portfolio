166531cfd3c78f1a5f7766b50205cd3cfe23bb1d
Mohammed Saeed <122816330+MohdMuslim92@users.noreply.github.com>
2025-10-31 20:10:44 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const BASE = '/my-portfolio/'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
